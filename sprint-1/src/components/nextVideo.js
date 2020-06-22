3015f3fe6d24c86015eea7f38d9d44244849567e
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2020-06-21 21:21:33 -0700

    Managed to render the video list content, having issues rendering the thumbnails though. Wasn't able to figure out how to render certain things, project will be submitted late

--- sprint-1/src/components/nextVideo.js
+++ sprint-1/src/components/nextVideo.js
@@ -4,7 +4,7 @@ import "../styles/nextVideo.scss";
 export default function NextVideo(props) {
   return (
     <div className="videos__nextVideo">
-      <div className="nextVideo__img">{props.img}</div>
+      <div className="nextVideo__img">{props.image}</div>
       <div className="nextVideo__text">
         <p className="nextVideo__title">{props.title}</p>
         <p className="nextVideo__by">{props.channel}</p>
--- sprint-1/src/components/videoList.js
+++ sprint-1/src/components/videoList.js
@@ -16,57 +16,9 @@ class VideoList extends Component {
             key={nextVideo.key}
             title={nextVideo.title}
             channel={nextVideo.channel}
-            // img={nextVideo.img}
+            image={nextVideo.image}
           />
         ))}
-        {/* <h5 className="videos__heading">NEXT VIDEO</h5>
-        <NextVideo
-          title="Become A Travel Pro In
-One Easy Lesson…"
-          by="Scotty Cranmer
-"
-        />
-        <NextVideo
-          title="Les Houches The
-Hidden Gem Of The…"
-          by="Scotty Cranmer
-"
-        />
-        <NextVideo
-          title="Travel Health Useful
-Medical Information…"
-          by="Scotty Cranmer
-"
-        />
-        <NextVideo
-          title="Cheap Airline Tickets
-Great Ways To Save"
-          by="Emily Harper
-"
-        />
-        <NextVideo
-          title="Take A Romantic Break
-In A Boutique Hotel"
-          by="Ethan Owen
-"
-        />
-        <NextVideo
-          title="Choose The Perfect
-Accommodations"
-          by="Lydia Perez
-"
-        />
-        <NextVideo
-          title="Cruising Destination
-Ideas"
-          by="Timothy Austin
-"
-        />
-        <NextVideo
-          title="Train Travel On Track
-For Safety"
-          by="Scotty Cranmer"
-        /> */}
       </section>
     );
   }
